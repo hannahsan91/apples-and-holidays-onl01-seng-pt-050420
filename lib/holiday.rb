77f1fef7e9a1bb0154c501b3c83cfdfec09b95b1
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 02:43:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash = {
-  :fourth_of_july => ["BBQ"]
+  :fourth_of_july => ["Fireworks", "BBQ"]
   :memorial_day => ["BBQ"]
 }
 end
