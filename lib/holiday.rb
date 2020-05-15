9c0e6b9783c3769eef6c4d7dbd04331ef764b8ea
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-15 02:46:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  winter_supplies(holiday_supplies)
+  :valentines_day => ["Cupid Cut-Out", "Candy Hearts"],
+}
 end
 
 
@@ -38,12 +39,14 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  :valentines_day => ["Cupid Cut-Out", "Candy Hearts"],
+  }
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  :valentines_day => ["Cupid Cut-Out", "Candy Hearts"],
+  }
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,6 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
 holiday_hash ={
   :winter => {
     :christmas => ["Lights", "Wreath"],
